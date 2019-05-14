c40b902c9df4f4bef5a25c88ba8cab54b75be9ac
tiva <752990270@qq.com>
2019-05-14 22:52:43 +0800

    template

--- src/main.js
+++ src/main.js
@@ -1,34 +1,8 @@
-/**
- * @file 入口
- * @author wangtianhua
- */
-define(function (require, exports, module) {
-
-  'use strict';
-
-  exports.init = function () {
-
-    var yox = new Yox({
-      
-      el: '#app',
-      template: require('tpl!./main.html'),
-
-      data: function () {
-        return {
-          
-        }
-      },
-
-
-      methods: {
-        
-      },
-
-      afterMount: function () {
-
-      }
-      
-    });
-
-  }
-});
+import Yox from 'yox'
+import './style/base.styl'
+import template from './main.html'
+console.log('>>>>', template)
+new Yox({
+  el: '#app',
+  template: template
+})
